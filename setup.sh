8b9cfc09b285aea86c97a6c4ebff3221ba5faef1
sdcgvhgj <1171556648@qq.com>
2023-06-20 18:51:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo "Please enter your domain name:"
-read domain_name
+#!/bin/bash
 
-echo "Please enter your password:"
+echo "password:"
 read password
-
-echo "DomainName: $domain_name"
-echo "Password: $password"
 
 set -x
 apt update
@@ -22,33 +18,32 @@ unzip trojan-go-linux-amd64.zip
 curl https://get.acme.sh | sh
 apt install socat
 ln -s /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
-acme.sh --register-account -m your_email@gmail.com
-acme.sh --issue -d $domain_name --standalone -k ec-256
-acme.sh --installcert -d $domain_name --ecc --key-file /root/trojan/server.key --fullchain-file /root/trojan/server.crt
-touch config.json
-echo '{
-  "run_type": "server",
-  "local_addr": "0.0.0.0",
-  "local_port": 10000,
-  "remote_addr": "127.0.0.1",
-  "remote_port": 80,
-  "password": ["$password"],
-  "ssl": {
-    "cert": "server.crt",
-    "key": "server.key",
-    "sni": "$domain_name"
-  }
-}' > config.json
+acme.sh --register-account -m sdcgvhgj@qq.com
+acme.sh --issue -d sdcgvhgj.top --standalone -k ec-256
+acme.sh --installcert -d sdcgvhgj.top --ecc --key-file /root/trojan/server.key --fullchain-file /root/trojan/server.crt
+echo "
+run-type: server
+local-addr: 0.0.0.0
+local-port: 10000
+remote-addr: 127.0.0.1
+remote-port: 80
+password:
+  - $password
+ssl:
+  cert: server.crt
+  key: server.key
+  sni: sdcgvhgj.top
+" > config.yaml
 apt install nginx
 nohup ./trojan-go > trojan.log 2>&1 &
-iptables -t nat -A PREROUTING -p tcp --dport 10000:11000 -j REDIRECT --to-port 10000
-iptables -t nat -A PREROUTING -p udp --dport 10000:11000 -j REDIRECT --to-port 10000
+iptables -t nat -A PREROUTING -p tcp --dport 10000:12000 -j REDIRECT --to-port 10000
+iptables -t nat -A PREROUTING -p udp --dport 10000:12000 -j REDIRECT --to-port 10000
 echo ----------Aria----------
 apt install aria2
 mkdir /root/.aria2
 touch /root/.aria2/aria2.session
 touch /root/.aria2/aria2.conf
-echo '
+echo "
 dir=/home/download
 input-file=/root/.aria2/aria2.session
 save-session=/root/.aria2/aria2.session
@@ -62,7 +57,7 @@ rpc-listen-all=true
 #rpc-secure=true
 #rpc-certificate=/root/trojan/server.cer
 #rpc-private-key=/root/trojan/server.key
-' > /root/.aria2/aria2.conf
+" > /root/.aria2/aria2.conf
 aria2c --daemon=true --conf-path=/root/.aria2/aria2.conf
 echo ----------Filebrowser----------
 cd /root
@@ -83,7 +78,7 @@ unzip AriaNg-1.3.3.zip
 echo '
 server {
 	listen 443 ssl;
-	server_name $domain_name;
+	server_name sdcgvhgj.top;
 	ssl_certificate /root/trojan/server.crt;
 	ssl_certificate_key /root/trojan/server.key;
 	root /var/www/html;
@@ -102,12 +97,11 @@ server {
 }
 server {
 	listen 80;
-	server_name $domain_name;
+	server_name sdcgvhgj.top;
 	location / {
 		rewrite ^(.*)$ https://$host$1 permanent;
 	}
  	location /a {
-  		root /var/www/html;
 		try_files $uri $uri/ =404;
 	}
 }
