673cf996fc43aad9185ba24d308ff34138d2f37c
sdcgvhgj <1171556648@qq.com>
2023-07-07 19:07:25 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,7 +34,6 @@ ssl:
   key: server.key
   sni: sdcgvhgj.top
 " > config.yaml
-apt install -y nginx
 nohup ./trojan-go > trojan.log 2>&1 &
 iptables -t nat -A PREROUTING -p tcp --dport 10000:12000 -j REDIRECT --to-port 10000
 iptables -t nat -A PREROUTING -p udp --dport 10000:12000 -j REDIRECT --to-port 10000
@@ -44,6 +43,7 @@ apt install -y aria2
 mkdir /root/.aria2
 touch /root/.aria2/aria2.session
 touch /root/.aria2/aria2.conf
+mkdir /home/download
 echo "
 dir=/home/download
 input-file=/root/.aria2/aria2.session
@@ -68,7 +68,7 @@ cd filebrowser
 wget https://github.com/filebrowser/filebrowser/releases/download/v2.23.0/linux-amd64-filebrowser.tar.gz
 tar -zxvf linux-amd64-filebrowser.tar.gz
 ./filebrowser config init
-./filebrowser config set --address 0.0.0.0 --baseurl '/f' --port 10001 --log ./filebrowser.log --root /home/download
+./filebrowser config set --address 0.0.0.0 --baseurl '/f' --port 10001 --log ./filebrowser.log --root /home
 ./filebrowser users add admin $password --perm.admin
 nohup ./filebrowser > /dev/null 2>&1 &
 
@@ -86,7 +86,23 @@ echo \
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo ----------NextCloud----------
+acme.sh --issue -d cloud.sdcgvhgj.top --standalone -k ec-256
+acme.sh --installcert -d cloud.sdcgvhgj.top --ecc --key-file /root/trojan/cloud.key --fullchain-file /root/trojan/cloud.crt
+docker run \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 8080:8080 \
+--env APACHE_PORT=10002 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+--env NEXTCLOUD_DATADIR="/home" \
+nextcloud/all-in-one:latest
+
 echo ----------Nginx----------
+apt install -y nginx
 cd /var/www/html
 mkdir a
 cd a
@@ -129,6 +145,56 @@ server {
 	}
 }
 ' > /etc/nginx/sites-enabled/default
+echo '
+server {
+    listen 80;
+    listen [::]:80;            # comment to disable IPv6
+
+    if ($scheme = "http") {
+        return 301 https://$host$request_uri;
+    }
+
+    listen 443 ssl http2;      # for nginx versions below v1.25.1
+    listen [::]:443 ssl http2; # for nginx versions below v1.25.1 - comment to disable IPv6
+
+    # listen 443 ssl;      # for nginx v1.25.1+
+    # listen [::]:443 ssl; # for nginx v1.25.1+ - keep comment to disable IPv6
+
+    server_name cloud.sdcgvhgj.top;
+
+    location / {
+        proxy_pass http://127.0.0.1:10002$request_uri;
+
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Port $server_port;
+        proxy_set_header X-Forwarded-Scheme $scheme;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header Accept-Encoding "";
+        proxy_set_header Host $host;
+    
+        client_body_buffer_size 512k;
+        proxy_read_timeout 86400s;
+        client_max_body_size 0;
+
+        # Websocket
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+    }
+
+    ssl_certificate /root/trojan/cloud.crt;
+    ssl_certificate_key /root/trojan/cloud.key;
+ 
+    ssl_session_timeout 1d;
+    ssl_session_cache shared:MozSSL:10m; # about 40000 sessions
+    ssl_session_tickets off;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+    ssl_prefer_server_ciphers on;
+}
+' > /etc/nginx/sites-enabled/cloud
 nginx -t
 systemctl start nginx
 systemctl reload nginx
